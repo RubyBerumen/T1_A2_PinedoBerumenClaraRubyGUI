fd6e882c80bc51cd7d992c3a2e84b6b0c9690597
Ruby Berumen <clararubipb15@gmail.com>
2021-02-05 17:47:09 -0600

    Se creó la clase VentanaFormulario con su constructor

--- src/FormularioLayoutNulo.java
+++ src/FormularioLayoutNulo.java
@@ -1,8 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+class VentanaFormulario extends JFrame{
+	
+	public VentanaFormulario(){
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(300,300);
+		setTitle("Formulario");
+		setLocationRelativeTo(null);
+		
+		
+		
+		
+		setVisible(true);
+	}
+	
+}
 
 public class FormularioLayoutNulo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
 
 	}
 
