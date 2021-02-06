88f6c03e287205b9d7886dec14ca70cebcb218ec
Ruby Berumen <clararubipb15@gmail.com>
2021-02-05 19:00:43 -0600

    Se agregó la parte de la esquina inferior izquierda

--- src/FormularioLayoutNulo.java
+++ src/FormularioLayoutNulo.java
@@ -6,7 +6,7 @@ class VentanaFormulario extends JFrame{
 	public VentanaFormulario(){
 		getContentPane().setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(900,600);
+		setSize(700,510);
 		setTitle("Formulario");
 		setLocationRelativeTo(null);
 		
@@ -19,16 +19,67 @@ class VentanaFormulario extends JFrame{
 		add(txt1);
 		
 		JLabel txt2 = new JLabel("Include the following:");
-		txt2.setBounds(25, 60, 150, 25);
+		txt2.setBounds(25, 50, 150, 25);
 		add(txt2);
 		
 		JCheckBox check1 = new JCheckBox("A title for your form");
-		check1.setBounds(25, 90, 150, 25);
+		check1.setBounds(25, 70, 150, 25);
 		add(check1);
 		JTextField txtF1 = new JTextField("Subscribe to our mailing list");
-		txtF1.setBounds(25, 120, 140, 25);
+		txtF1.setBounds(25, 100, 200, 25);
 		add(txtF1);
 		
+		
+		ButtonGroup bg = new ButtonGroup();
+		JRadioButton radio1 = new JRadioButton("only required fields");
+		radio1.setBounds(25, 150, 150, 25);
+		bg.add(radio1);
+		add(radio1);
+		JRadioButton radio2 = new JRadioButton("all fields");
+		radio2.setBounds(25, 170, 150, 25);
+		bg.add(radio2);
+		add(radio2);
+		
+		
+		JCheckBox check2 = new JCheckBox("interest group fields");
+		check2.setBounds(25, 210, 150, 25);
+		add(check2);
+		JCheckBox check3 = new JCheckBox("required field indicators");
+		check3.setBounds(25, 230, 150, 25);
+		add(check3);
+		
+		
+		JLabel txt3 = new JLabel("Set form width");
+		txt3.setBounds(25, 270, 150, 25);
+		add(txt3);
+		JTextField txtF2 = new JTextField();
+		txtF2.setBounds(25, 300, 200, 25);
+		add(txtF2);
+		
+		
+		JLabel txt4 = new JLabel("Enhance your form");
+		txt4.setBounds(25, 350, 150, 25);
+		add(txt4);
+		JCheckBox check4 = new JCheckBox("enable evil popup mode");
+		check4.setBounds(25, 370, 150, 25);
+		add(check4);
+		JCheckBox check5 = new JCheckBox("disable all");
+		check5.setBounds(25, 390, 150, 25);
+		add(check5);
+		JCheckBox check6 = new JCheckBox("include archive link");
+		check6.setBounds(25, 410, 150, 25);
+		add(check6);
+		JCheckBox check7 = new JCheckBox("include page link");
+		check7.setBounds(25, 430, 150, 25);
+		add(check7);
+		
+		
+		JLabel txt5 = new JLabel("Preview");
+		txt5.setBounds(260, 30, 150, 25);
+		add(txt5);
+		JLabel txt6 = new JLabel("Email Address");
+		txt6.setBounds(260, 50, 150, 25);
+		add(txt6);
 		
 		
 		setVisible(true);
