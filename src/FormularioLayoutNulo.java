1a9508c8fa44310fe1aaa892f085fac55f12be11
Ruby Berumen <clararubipb15@gmail.com>
2021-02-05 20:00:05 -0600

    Se agrgó la parte de la esquina inferior derecha

--- src/FormularioLayoutNulo.java
+++ src/FormularioLayoutNulo.java
@@ -6,12 +6,12 @@ class VentanaFormulario extends JFrame{
 	public VentanaFormulario(){
 		getContentPane().setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(600,510);
+		setSize(600,500);
 		setTitle("Formulario");
 		setLocationRelativeTo(null);
 		
 		JLabel txtTitulo = new JLabel("<<<Formulario de registro>>>");
-		txtTitulo.setBounds(25, 0, 300, 20);
+		txtTitulo.setBounds(25, 5, 300, 20);
 		add(txtTitulo);
 		
 		JLabel txt1 = new JLabel("From option");
@@ -19,14 +19,14 @@ class VentanaFormulario extends JFrame{
 		add(txt1);
 		
 		JLabel txt2 = new JLabel("Include the following:");
-		txt2.setBounds(25, 50, 150, 25);
+		txt2.setBounds(25, 60, 150, 25);
 		add(txt2);
 		
 		JCheckBox check1 = new JCheckBox("A title for your form");
-		check1.setBounds(25, 70, 150, 25);
+		check1.setBounds(25, 80, 150, 25);
 		add(check1);
 		JTextField txtF1 = new JTextField("Subscribe to our mailing list");
-		txtF1.setBounds(25, 100, 200, 25);
+		txtF1.setBounds(25, 105, 200, 25);
 		add(txtF1);
 		
 		
@@ -53,24 +53,24 @@ class VentanaFormulario extends JFrame{
 		txt3.setBounds(25, 270, 150, 25);
 		add(txt3);
 		JTextField txtF2 = new JTextField();
-		txtF2.setBounds(25, 300, 200, 25);
+		txtF2.setBounds(25, 290, 200, 25);
 		add(txtF2);
 		
 		
 		JLabel txt4 = new JLabel("Enhance your form");
-		txt4.setBounds(25, 350, 150, 25);
+		txt4.setBounds(25, 340, 150, 25);
 		add(txt4);
 		JCheckBox check4 = new JCheckBox("enable evil popup mode");
-		check4.setBounds(25, 370, 150, 25);
+		check4.setBounds(25, 360, 150, 25);
 		add(check4);
 		JCheckBox check5 = new JCheckBox("disable all");
-		check5.setBounds(25, 390, 150, 25);
+		check5.setBounds(25, 380, 150, 25);
 		add(check5);
 		JCheckBox check6 = new JCheckBox("include archive link");
-		check6.setBounds(25, 410, 150, 25);
+		check6.setBounds(25, 400, 150, 25);
 		add(check6);
 		JCheckBox check7 = new JCheckBox("include page link");
-		check7.setBounds(25, 430, 150, 25);
+		check7.setBounds(25, 420, 150, 25);
 		add(check7);
 		
 		
@@ -79,29 +79,48 @@ class VentanaFormulario extends JFrame{
 		add(txt5);
 		
 		JLabel txt6 = new JLabel("Email Address");
-		txt6.setBounds(260, 60, 150, 25);
+		txt6.setBounds(270, 60, 150, 25);
 		add(txt6);
 		JTextField txtF3 = new JTextField();
-		txtF3.setBounds(260, 80, 300, 25);
+		txtF3.setBounds(270, 80, 300, 25);
 		add(txtF3);
 		
 		JLabel txt7 = new JLabel("First Name");
-		txt7.setBounds(260, 110, 150, 25);
+		txt7.setBounds(270, 110, 150, 25);
 		add(txt7);
 		JTextField txtF4 = new JTextField();
-		txtF4.setBounds(260, 130, 300, 25);
+		txtF4.setBounds(270, 130, 300, 25);
 		add(txtF4);
 		
 		JLabel txt8 = new JLabel("Last Name");
-		txt8.setBounds(260, 160, 150, 25);
+		txt8.setBounds(270, 160, 150, 25);
 		add(txt8);
 		JTextField txtF5 = new JTextField();
-		txtF5.setBounds(260, 180, 300, 25);
+		txtF5.setBounds(270, 180, 300, 25);
 		add(txtF5);
 		
 		JButton btn = new JButton("Subscribe");
-		btn.setBounds(260, 220, 90, 25);
+		btn.setBounds(270, 220, 90, 25);
 		add(btn);
+		
+		
+		JLabel txt9 = new JLabel("Copy/paste onto your site");
+		txt9.setBounds(260, 270, 150, 25);
+		add(txt9);
+		
+        JTextArea textField = new JTextArea();
+        textField.setEditable(false);
+
+        String str = "";
+        for (int i = 0; i < 60; ++i)
+            str += "Hola\n";
+        textField.setText(str);
+
+        JScrollPane scroll = new JScrollPane(textField);
+        scroll.setBounds(260, 295, 300, 140);                    
+
+        getContentPane().add(scroll);
+        setLocationRelativeTo ( null );
 		
 		
 		setVisible(true);
